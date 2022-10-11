49334f3f9698bffbd2ceb134296c457a88d46e86
Bhagyashri Laddha <bhagyashri.laddha12@gmail.com>
2022-10-11 10:17:07 +0200

    11-10-2022 All navbar updated

--- src/main/java/com/example/GraduationProject/Presentation/HomeController.java
+++ src/main/java/com/example/GraduationProject/Presentation/HomeController.java
@@ -8,13 +8,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
 @Controller
 public class HomeController
 {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home()
     {
         return "home";
